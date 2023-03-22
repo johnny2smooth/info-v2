3e0ca057dacf26c0ba28f87c3abbd2129e919c94
johnny2smooth <johnbp13@gmail.com>
2023-03-22 10:42:41 -0700

    piped dictionary into about

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,24 +1,38 @@
 import { Contact } from "../general";
 import Image from "next/image";
+import { getDictionary } from "@/get-dictionary";
+import type { Locale } from "@/i18n-config";
 
-export default function Page() {
+type TeamMember = {
+  firstName: string;
+  lastName: string;
+  image: string;
+  role: string;
+  description: string;
+};
+
+export default async function Page({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
+  const { about, team } = dictionary;
   return (
     <>
       <div className="p-4 stack">
-        <h2 className="text-[#003587] text-3xl">Who we are</h2>
-        <Team />
-        <h2 className="text-[#003587] text-3xl">What we do</h2>
-        <p>
-          All about our mission + more. What have we learned along the way and
-          where do we see this project in 1 year
-        </p>
+        <h2 className="text-[#003587] text-3xl">{about.title}</h2>
+        <TeamPhotos teamMembers={team.members} />
+        <h2 className="text-[#003587] text-3xl">{about.subtitle}</h2>
+        <p>{about.description}</p>
+        {}
       </div>
       <Contact />
     </>
   );
 }
 
-const Team = () => {
+const TeamPhotos = ({ teamMembers }: { teamMembers: TeamMember[] }) => {
   return (
     <div className="flex flex-wrap gap-2">
       {teamMembers.map((member, i) => (
@@ -35,70 +49,3 @@ const Team = () => {
     </div>
   );
 };
-
-let teamMembers = [
-  {
-    firstName: "Jojo",
-    lastName: "Bizarro",
-    image: "jojo",
-    role: "CEO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "Meme",
-    lastName: "Lord",
-    image: "meme",
-    role: "CTO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "John",
-    lastName: "Doe",
-    image: "john",
-    role: "CFO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    image: "jane",
-    role: "CMO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "Jojo",
-    lastName: "Bizarro",
-    image: "jojo",
-    role: "CEO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "Meme",
-    lastName: "Lord",
-    image: "meme",
-    role: "CTO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "John",
-    lastName: "Doe",
-    image: "john",
-    role: "CFO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    image: "jane",
-    role: "CMO",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-  },
-];
